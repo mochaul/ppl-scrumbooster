8f7c3c6b5a4b0f2fb42b621d979899893e7e1196
izzan.fakhril <izzan.fakhril@ui.ac.id>
2019-02-26 06:35:27 +0700

    [CHORES] Edit start.sh to run auto-deployment to Visual Studio AppCenter

--- start.sh
+++ start.sh
@@ -2,4 +2,10 @@
 
 echo Flutter Doctor in progress...
 
-flutter doctor -v
+npm install appcenter-cli
+
+flutter build apk --build-name=Scrum-Booster --build-number=1
+
+appcenter login --token $APP_CENTER_API_KEY
+
+appcenter distribute release -f build/app/outputs/apk/release/app-release.apk --app $APP_CENTER_APP_NAME --group $APP_CENTER_GROUP_TARGET
