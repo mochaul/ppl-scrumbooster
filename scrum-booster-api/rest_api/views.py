8ea02b6d90539544844c5a8e7d78be7bb80c76f9
luqi <khoiruluqi@gmail.com>
2019-02-26 06:31:38 +0700

    [GREEN] Membuat API

--- scrum-booster-api/rest_api/views.py
+++ scrum-booster-api/rest_api/views.py
@@ -1,21 +1,28 @@
 from django.shortcuts import render
 from rest_framework import viewsets
+from . import serializers, models
 
 # Create your views here.
 class PhaseViewSet(viewsets.ModelViewSet) :
-    pass
+    queryset = models.Phase.objects.all()
+    serializer_class = serializers.PhaseSerializer
 
 class CMMIPracticesViewSet(viewsets.ModelViewSet) :
-    pass
+    queryset = models.CMMIPractices.objects.all()
+    serializer_class = serializers.CMMIPracticesSerializer
 
 class CeremonyViewSet(viewsets.ModelViewSet) :
-    pass
+    queryset = models.Ceremony.objects.all()
+    serializer_class = serializers.CeremonySerializer
 
 class ProblemViewSet(viewsets.ModelViewSet) :
-    pass
+    queryset = models.Problem.objects.all()
+    serializer_class = serializers.ProblemSerializer
 
 class GlossaryViewSet(viewsets.ModelViewSet) :
-    pass
+    queryset = models.Glossary.objects.all()
+    serializer_class = serializers.GlossarySerializer
 
 class QuizQuestionViewSet(viewsets.ModelViewSet) :
-    pass
+    queryset = models.QuizQuestion.objects.all()
+    serializer_class = serializers.QuizQuestionSerializer
