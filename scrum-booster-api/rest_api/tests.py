31422fd9c80d0d8216603adc8c1cd1ac5213b6a2
luqi <khoiruluqi@gmail.com>
2019-02-26 06:24:29 +0700

    [RED] Membuat test untuk API

--- scrum-booster-api/rest_api/tests.py
+++ scrum-booster-api/rest_api/tests.py
@@ -1,3 +1,32 @@
 from django.test import TestCase
+from rest_framework.test import APITestCase
+from django.urls import reverse
+from rest_framework import status
 
 # Create your tests here.
+
+class ScrumBoosterTest(APITestCase):
+
+    def test_phase_url_exists(self):
+        response = self.client.get(reverse("phase-list"))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_cmmi_practices_url_exists(self):
+        response = self.client.get(reverse("cmmi-practices-list"))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_ceremony_url_exists(self):
+        response = self.client.get(reverse("ceremony-list"))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_problem_url_exists(self):
+        response = self.client.get(reverse("problem-list"))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_glossary_url_exists(self):
+        response = self.client.get(reverse("glossary-list"))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_quiz_question_url_exists(self):
+        response = self.client.get(reverse("quiz-question-list"))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
--- scrum-booster-api/rest_api/views.py
+++ scrum-booster-api/rest_api/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from rest_framework import viewsets
 
 # Create your views here.
+class PhaseViewSet(viewsets.ModelViewSet) :
+    pass
+
+class CMMIPracticesViewSet(viewsets.ModelViewSet) :
+    pass
+
+class CeremonyViewSet(viewsets.ModelViewSet) :
+    pass
+
+class ProblemViewSet(viewsets.ModelViewSet) :
+    pass
+
+class GlossaryViewSet(viewsets.ModelViewSet) :
+    pass
+
+class QuizQuestionViewSet(viewsets.ModelViewSet) :
+    pass
