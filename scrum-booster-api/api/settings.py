890857e222ceba32fe88063b7d8d03b4c37506f5
Wildan Fahmi Gunawan <wildanfahmigunawan@gmail.com>
2019-02-25 13:38:08 +0700

    setup landing page django rest api

--- scrum-booster-api/api/settings.py
+++ scrum-booster-api/api/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'scrumboosterapi',
 ]
 
 MIDDLEWARE = [
@@ -118,3 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}
--- scrum-booster-api/api/urls.py
+++ scrum-booster-api/api/urls.py
@@ -14,11 +14,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-
+from django.urls import path, include
+from rest_framework import routers
+from scrumboosterapi import views as scrumviews
 from . import views
+
+router = routers.DefaultRouter()
+router.register(r'users', scrumviews.UserViewSet)
+router.register(r'groups', scrumviews.GroupViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index, name='index'),
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
