00c660a22ebdede45e3886404692a17e8c459750
izzan.fakhril <izzan.fakhril@ui.ac.id>
2019-02-28 20:09:07 +0700

    [CHORES] changing AppCenter build name flag

--- start.sh
+++ start.sh
@@ -4,6 +4,8 @@ npm install --global appcenter-cli
 
 flutter build apk --build-name=scrum-booster --build-number=1
 
+appcenter
+
 appcenter login --token $APP_CENTER_API_KEY
 
 appcenter distribute release -f build/app/outputs/apk/release/app-release.apk --app $APP_CENTER_APP_NAME --group $APP_CENTER_GROUP_TARGET
