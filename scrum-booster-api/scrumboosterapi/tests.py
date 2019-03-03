703cf87d02e0a5e28214d8f57e0b162fb156674d
Wildan Fahmi Gunawan <wildanfahmigunawan@gmail.com>
2019-03-04 00:57:19 +0700

    [CHORUS] added test and test CI

--- scrum-booster-api/scrumboosterapi/tests.py
+++ scrum-booster-api/scrumboosterapi/tests.py
@@ -1,3 +1,10 @@
-from django.test import TestCase
+from django.test import TestCase,Client
 
-# Create your tests here.
+# Create your tests here. 
+class DjangoRestApiTest(TestCase):
+    client = Client()
+
+    def test(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
