185400e423ba96b07e8c332b02e5183fd05c361e
izzan.fakhril <izzan.fakhril@ui.ac.id>
2019-03-05 12:09:15 +0700

    [CHORES] Merge with US-2 Branch

--- scrum-booster-api/rest_api/urls.py
+++ scrum-booster-api/rest_api/urls.py
@@ -4,6 +4,8 @@ from . import views
 
 router = routers.DefaultRouter()
 router.register(r'phase', views.PhaseViewSet, base_name="phase")
+router.register(r'process-area', views.ProcessAreaViewSet, base_name="process-area")
+router.register(r'goal', views.GoalViewSet, base_name="goal")
 router.register(r'cmmi-practices', views.CMMIPracticesViewSet, base_name="cmmi-practices")
 router.register(r'ceremony', views.CeremonyViewSet, base_name="ceremony")
 router.register(r'problem', views.ProblemViewSet, base_name="problem")
