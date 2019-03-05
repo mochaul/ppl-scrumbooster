a8e39349d2356aa1195be99c3a269c45cd985ec8
luqi <khoiruluqi@gmail.com>
2019-03-05 23:44:03 +0700

    [GREEN] Resolve Conflict

--- scrum-booster-api/api/urls.py
+++ scrum-booster-api/api/urls.py
@@ -14,21 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-from rest_framework import routers
-from scrumboosterapi import views as scrumviews
-from . import views
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-
-
-router = routers.DefaultRouter()
-router.register(r'users', scrumviews.UserViewSet)
-router.register(r'groups', scrumviews.GroupViewSet)
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    path('', include('rest_api.urls'), name='api'),
 ]
 
 urlpatterns += staticfiles_urlpatterns()
