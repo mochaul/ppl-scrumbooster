8417163f18ca9e71b72334e7059a6832a1bd8613
Muhammad Rezki <iskarimrezki@gmail.com>
2019-03-08 20:39:37 +0700

    [RED] Add Makara UI to About page

--- lib/initScreen/AboutPage.dart
+++ lib/initScreen/AboutPage.dart
@@ -6,7 +6,7 @@ class AboutPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("About Scrum Booster", style: TextStyle(color: Colors.white),),
+        title: Text("About Scrum Booster", style: TextStyle(color: Colors.purpleAccent),),
       ),
       
       //add new cotainer for page's contents
@@ -34,47 +34,11 @@ class AboutPage extends StatelessWidget {
 
                 //import University logo from the assets
                 new Image.asset(
-                  'assets/logos/Makar-UI.png', 
+                  'assets/logos/Makara-UI.png', 
                     height: 120,
                   ),
               ], 
             ),
-
-
-            //add padding for content
-            Padding(
-              padding: EdgeInsets.only(top: 50.0),
-              child: new Column(
-                children: <Widget>[
-
-                    //about content
-                    new Text('Scrum Booster is a software project developed by RSE Labs. RSE Labs is a part of Faculty of Computer Science in University of Indonesia. Scrum Booster is part of software engineering projects by:',
-                    textAlign: TextAlign.center, style: TextStyle(fontSize: 16,)),
-
-                    Padding(padding: EdgeInsets.all(10.0),), // add space between 
-
-                    //name for each members
-                    new Text('Mochamad Aulia Akbar Praditomo',
-                    textAlign: TextAlign.center, style: TextStyle(fontSize: 16,)),
-
-                    new Text('Fajrin Maulana Kingwijati',
-                    textAlign: TextAlign.center, style: TextStyle(fontSize: 16,)),
-
-                    new Text('Izzan Fakhril Islam',
-                    textAlign: TextAlign.center, style: TextStyle(fontSize: 16,)),
-
-                    new Text('Muhammad Rezki',
-                    textAlign: TextAlign.center, style: TextStyle(fontSize: 16,)),
-
-                    new Text('Khoirul Kholuqi',
-                    textAlign: TextAlign.center, style: TextStyle(fontSize: 16,)),
-
-                    new Text('Wildan Fahmi Gunawan',
-                    textAlign: TextAlign.center, style: TextStyle(fontSize: 16,)),
-                ],
-
-              ) 
-            )
           ],
         ),
       )
