22e47f672ff014e15b480e406efd65a142874c31
Mochamad Aulia Akbar Praditomo <praditomo@gmail.com>
2019-03-11 23:33:07 +0700

    [RED] add routing from main

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:ScrumBooster/utils/utils.dart';
 import 'package:ScrumBooster/initScreen/splashScreen.dart';
 import 'package:flutter/services.dart';
 import 'package:ScrumBooster/initScreen/HomeScreen.dart';
+import 'package:ScrumBooster/contentsList/ListProblems.dart';
 
 void main() {
   SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
@@ -37,6 +38,7 @@ class _ScrumBoosterAppState extends State<ScrumBoosterApp> {
       home: new SplashScreen(),
       routes: <String, WidgetBuilder>{
         '/Home': (BuildContext context) => new HomePage(),
+        '/ListProblems': (BuildContext context) => new ListProblems(),
       },
     );
   }
