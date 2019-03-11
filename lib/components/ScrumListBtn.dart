3a26e3f009ea2f498d4b90787c3a0e04d4e8eca7
Fajrin Maulana <fajrinmk@gmail.com>
2019-03-12 02:33:43 +0700

    [RED] Implementasi test product backlog

---
+++ lib/components/ScrumListBtn.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:ScrumBooster/utils/utils.dart';
+
+class ScrumListBtn extends StatelessWidget {
+  final String title;
+  final VoidCallback action;
+  final String imgUrl;
+
+  ScrumListBtn({
+    Key key,
+    @required this.title,
+    @required this.action,
+    @required this.imgUrl,
+  }) : super(key: key);
+
+  var util = new Util();
+
+  @override
+  Widget build(BuildContext context) {
+    double _height = MediaQuery.of(context).size.height;
+    double _width = MediaQuery.of(context).size.width;
+
+    return Ink.image(
+      image: AssetImage(this.imgUrl),
+      fit: BoxFit.cover,
+      width: 120.0,
+      height: 120.0,
+      child: InkWell(
+          onTap: this.action,
+      child: null,
+    ),
+    );
+  }
+}
