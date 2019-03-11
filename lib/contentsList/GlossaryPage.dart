cd36b8953802ab0372bc0bfd4a2266d4d6201a1e
Muhammad Rezki <iskarimrezki@gmail.com>
2019-03-11 23:07:47 +0700

    [GREEN] add dummy content to glossary

--- lib/contentsList/GlossaryPage.dart
+++ lib/contentsList/GlossaryPage.dart
@@ -24,22 +24,48 @@ var utils = new Util();
       body: Center(
         child: ListView(
           children: <Widget>[
-            Padding(padding: EdgeInsets.all(10.0),),
-            Text("GLOSSARY", style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold), textAlign: TextAlign.center,),
-            Padding(padding: EdgeInsets.all(10.0),),
+            Padding(padding: EdgeInsets.all(10),),
+            Text("CEREMONIES", style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold), textAlign: TextAlign.center,),
+            Padding(padding: EdgeInsets.all(10),),
             Image.asset("assets/listGlossary/glossary.png", height: 200,),
             Container(
-              padding :EdgeInsets.all(10),
-              child : Column(
+              padding: EdgeInsets.all(20),
+              child: Column(
                 mainAxisAlignment: MainAxisAlignment.start,
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: <Widget>[
-                Text("A", style: TextStyle(fontSize: 46, fontWeight: FontWeight.w500),),
-                Text("B", style: TextStyle(fontSize: 46, fontWeight: FontWeight.w500),),
-                Text("C", style: TextStyle(fontSize: 46, fontWeight: FontWeight.w500),),
+                  Text("A", style: TextStyle(fontSize: 46, fontWeight: FontWeight.w500),),
+                  Padding(
+                    padding: EdgeInsets.all(10),
+                    child: Column(
+                      children: <Widget>[
+                        Text("A dummy 1", style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500, color: utils.hexToColor("#3498DB")),),
+                        Text("A dummy 2", style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500, color: utils.hexToColor("#3498DB")),),
+                        Text("A dummy 3", style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500, color: utils.hexToColor("#3498DB")),),
+                      ],
+                    ),),
+                  Text("B", style: TextStyle(fontSize: 46, fontWeight: FontWeight.w500),),
+                  Padding(
+                    padding: EdgeInsets.all(10),
+                    child: Column(
+                      children: <Widget>[
+                        Text("B dummy 1", style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500, color: utils.hexToColor("#3498DB")),),
+                        Text("B dummy 2", style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500, color: utils.hexToColor("#3498DB")),),
+                        Text("B dummy 3", style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500, color: utils.hexToColor("#3498DB")),),
+                      ],
+                    ),),
+                  Text("C", style: TextStyle(fontSize: 46, fontWeight: FontWeight.w500),),
+                  Padding(
+                    padding: EdgeInsets.all(10),
+                    child: Column(
+                      children: <Widget>[
+                        Text("C dummy 1", style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500, color: utils.hexToColor("#3498DB")),),
+                        Text("C dummy 2", style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500, color: utils.hexToColor("#3498DB")),),
+                        Text("C dummy 3", style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500, color: utils.hexToColor("#3498DB")),),
+                      ],
+                    ),),
                 ],
-                
-              )
+              ),
             )
           ],
         )
