46bb096faa3f6592d0e3adc86d4852a0a56a807b
izzan.fakhril <izzan.fakhril@ui.ac.id>
2019-03-24 20:01:28 +0700

    [CHORES] Home Menu visual fixes

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -107,31 +107,6 @@ void main() {
     expect(find.text(menu1), findsOneWidget);
   });
 
-  // testWidgets('Ceremonies Content Drawer Test', (WidgetTester tester) async {
-  //   GlobalKey<ScaffoldState> scaffoldKey = home.getScaffoldKey();
-  //   await tester.pumpWidget(makeTestableWidget(child: ceremonies));
-
-  //   const menu1 = "Home";
-  //   const menu2 = "Ceremonies";
-  //   const menu3 = "Problems";
-  //   const menu4 = "Glossary";
-  //   const menu5 = "Pop Quiz!";
-  //   const menu6 = "About";
-
-  //   // Tap the 'burger menu' icon and trigger a frame.
-  //   scaffoldKey.currentState.openDrawer();
-  //   print("berhasil yey");
-  //   await tester.pump();
-
-  //   // Verify all the sidebar menu.
-  //   expect(find.text(menu1), findsOneWidget);
-  //   expect(find.text(menu2), findsOneWidget);
-  //   expect(find.text(menu3), findsOneWidget);
-  //   expect(find.text(menu4), findsOneWidget);
-  //   expect(find.text(menu5), findsOneWidget);
-  //   expect(find.text(menu6), findsOneWidget);
-  // });
-
   testWidgets('Test Find Title Ceremonies', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(makeTestableWidget(child: ceremonies));
