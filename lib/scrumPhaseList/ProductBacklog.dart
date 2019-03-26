ff6aa847b0d2d03c9e765eb6988d36fde93cbfaf
izzan.fakhril <izzan.fakhril@ui.ac.id>
2019-03-26 14:19:38 +0700

    [REFACTOR] implementing scrum phase content button component in scrum phase page

--- lib/scrumPhaseList/ProductBacklog.dart
+++ lib/scrumPhaseList/ProductBacklog.dart
@@ -3,6 +3,7 @@ import 'package:ScrumBooster/utils/utils.dart';
 import 'package:ScrumBooster/components/ScrumListBtn.dart';
 import 'package:ScrumBooster/contents/ceremonies.dart';
 import 'package:ScrumBooster/contents/problems.dart';
+import 'package:ScrumBooster/components/ScrumPhaseContentBtn.dart';
 
 class ProductBacklog extends StatelessWidget {
   final List<String> homeImageList = new List();
@@ -79,119 +80,59 @@ class ProductBacklog extends StatelessWidget {
             color: utils.hexToColor("#3498DB"),
           ),
           new Row(
-            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: <Widget>[
               new Row(
                 children: <Widget>[
-                  new Column(
-                    children: <Widget>[
-                      new ScrumListBtn(
-                        title: "Backlog Grooming",
-                        action: (){
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => Ceremonies(
-                                imagePath: "assets/scrumPhase/content_a.png",
-                                title: "BACKLOG GROOMING",
-                                contents: "Backlog grooming is when the product owner and some, or all, of the rest of the team review items on the backlog to ensure the backlog contains the appropriate items, that they are prioritized, and that the items at the top of the backlog are ready for delivery.",
-                              )
-                            ),
-                          );
-                        },
-                        imgUrl: "assets/scrumPhase/content_a.png",
-                      ),
-                      new Text(
-                        "Backlog Grooming",
-                        textAlign: TextAlign.center,
-                      )
-                    ],
+                  new ScrumPhaseContentBtn(
+                    title: "Backlog Grooming",
+                    imageAssetURL: "assets/scrumPhase/content_a.png",
+                    action: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => Ceremonies(
+                            imagePath: "assets/scrumPhase/content_a.png",
+                            title: "BACKLOG GROOMING",
+                            contents: "Backlog grooming is when the product owner and some, or all, of the rest of the team review items on the backlog to ensure the backlog contains the appropriate items, that they are prioritized, and that the items at the top of the backlog are ready for delivery.",
+                          )
+                        ),
+                      );
+                    },
                   ),
-                  new Padding(
-                    padding: EdgeInsets.all(12),
+                  new ScrumPhaseContentBtn(
+                    title: "content b",
+                    imageAssetURL: "assets/scrumPhase/content_b.png",
+                    action: () {}, //TODO: Implement masuk ke page content b
                   ),
-                  new Column(
-                    children: <Widget>[
-                      new ScrumListBtn(
-                        title: "Content B",
-                        action: () => {},
-                        imgUrl: "assets/scrumPhase/content_b.png",
-                      ),
-                      new Text(
-                        "content b",
-                        textAlign: TextAlign.center,
-                      ),
-                    ],
-                  ),
-                  new Padding(
-                    padding: EdgeInsets.all(12),
-                  ),
-                  new Column(
-                    children: <Widget>[
-                      new ScrumListBtn(
-                        title: "Content C",
-                        action: () => {},
-                        imgUrl: "assets/scrumPhase/content_c.png",
-                      ),
-                      new Text(
-                        "content c",
-                        textAlign: TextAlign.center,
-                      ),
-                    ],
+                  new ScrumPhaseContentBtn(
+                    title: "content c",
+                    imageAssetURL: "assets/scrumPhase/content_c.png",
+                    action: () {} //TODO: Implement masuk ke page content c
                   ),
                 ],
               ),
             ],
           ),
           new Row(
-            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: <Widget>[
               new Row(
                 children: <Widget>[
-                  new Column(
-                    children: <Widget>[
-                      new ScrumListBtn(
-                        title: "Content D",
-                        action: () => {},
-                        imgUrl: "assets/scrumPhase/content_d.png",
-                      ),
-                      new Text(
-                        "content d",
-                        textAlign: TextAlign.center,
-                      )
-                    ],
+                  new ScrumPhaseContentBtn(
+                    title: "content d",
+                    imageAssetURL: "assets/scrumPhase/content_d.png",
+                    action: () {},
                   ),
-                  new Padding(
-                    padding: EdgeInsets.all(12),
+                  new ScrumPhaseContentBtn(
+                    title: "content e",
+                    imageAssetURL: "assets/scrumPhase/content_e.png",
+                    action: () {},
                   ),
-                  new Column(
-                    children: <Widget>[
-                      new ScrumListBtn(
-                        title: "Content E",
-                        action: () => {},
-                        imgUrl: "assets/scrumPhase/content_e.png",
-                      ),
-                      new Text(
-                        "content e",
-                        textAlign: TextAlign.center,
-                      )
-                    ],
-                  ),
-                  new Padding(
-                    padding: EdgeInsets.all(12),
-                  ),
-                  new Column(
-                    children: <Widget>[
-                      new ScrumListBtn(
-                        title: "Content F",
-                        action: () => {},
-                        imgUrl: "assets/scrumPhase/content_a.png",
-                      ),
-                      new Text(
-                        "content f",
-                        textAlign: TextAlign.center,
-                      ),
-                    ],
+                  new ScrumPhaseContentBtn(
+                    title: "content f",
+                    imageAssetURL: "assets/scrumPhase/content_a.png",
+                    action: () {},
                   ),
                 ],
               ),
@@ -217,125 +158,63 @@ class ProductBacklog extends StatelessWidget {
             color: utils.hexToColor("#E74D3B"),
           ),
           new Row(
-            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: <Widget>[
               new Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: <Widget>[
-                  new Column(
-                    children: <Widget>[
-                      new ScrumListBtn(
-                        title: "Problem A",
-                        action: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => ProblemsContentPage(
-                                imagePath: "assets/scrumPhase/problem_a.png",
-                                title: "POOR PROJECT COMMUNICATION",
-                                contents: "One of the primary advantages of adopting agile approaches is the superior way it deals with real-time communications. Agile ceremonies and techniques such as Release / Sprint Planning, Daily Stand Up, Spring Demo / Reviews, and Retrospectives encourage communication between team members on a daily, incremental basis. Any additional decisions regarding frequency of communication can be captured in the Team Agreement.",
-                              )
-                            ),
-                          );
-                        },
-                        imgUrl: "assets/scrumPhase/problem_a.png",
-                      ),
-                      new Container(
-                        height: 10.5,
-                        width: 150.0,
-                        child: new Text(
-                        "Poor Project Communication",
-                        textAlign: TextAlign.center,
-                        )
-                      ),
-                    ],
+                  new ScrumPhaseContentBtn(
+                    title: "Poor Project Communication",
+                    imageAssetURL: "assets/scrumPhase/problem_a.png",
+                    action: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => ProblemsContentPage(
+                            imagePath: "assets/scrumPhase/problem_a.png",
+                            title: "POOR PROJECT COMMUNICATION",
+                            contents: "One of the primary advantages of adopting agile approaches is the superior way it deals with real-time communications. Agile ceremonies and techniques such as Release / Sprint Planning, Daily Stand Up, Spring Demo / Reviews, and Retrospectives encourage communication between team members on a daily, incremental basis. Any additional decisions regarding frequency of communication can be captured in the Team Agreement.",
+                          )
+                        ),
+                      );
+                    },
                   ),
-                  new Padding(
-                    padding: EdgeInsets.all(12),
+                  new ScrumPhaseContentBtn(
+                    title: "problem b",
+                    imageAssetURL: "assets/scrumPhase/problem_b.png",
+                    action: () {},
                   ),
-                  new Column(
-                    children: <Widget>[
-                      new ScrumListBtn(
-                        title: "Problem B",
-                        action: () => {},
-                        imgUrl: "assets/scrumPhase/problem_b.png",
-                      ),
-                      new Text(
-                        "problem b",
-                        textAlign: TextAlign.center,
-                      )
-                    ],
-                  ),
-                  new Padding(
-                    padding: EdgeInsets.all(12),
-                  ),
-                  new Column(
-                    children: <Widget>[
-                      new ScrumListBtn(
-                        title: "Problem C",
-                        action: () => {},
-                        imgUrl: "assets/scrumPhase/problem_c.png",
-                      ),
-                      new Text(
-                        "problem c",
-                        textAlign: TextAlign.center,
-                      )
-                    ],
+                  new ScrumPhaseContentBtn(
+                    title: "problem c",
+                    imageAssetURL: "assets/scrumPhase/problem_c.png",
+                    action: () {},
                   ),
                 ],
               ),
             ],
           ),
           new Row(
-            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: <Widget>[
               new Padding(
                 padding: EdgeInsets.only(top: 25.0),
                 child: new Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                   children: <Widget>[
-                    new Column(
-                      children: <Widget>[
-                        new ScrumListBtn(
-                          title: "Problem D",
-                          action: () => {},
-                          imgUrl: "assets/scrumPhase/problem_d.png",
-                        ),
-                        new Text(
-                          "problem d",
-                          textAlign: TextAlign.center,
-                        )
-                      ],
+                    new ScrumPhaseContentBtn(
+                      title: "problem d",
+                      imageAssetURL: "assets/scrumPhase/problem_d.png",
+                      action: () {},
                     ),
-                    new Padding(
-                      padding: EdgeInsets.all(12),
+                    new ScrumPhaseContentBtn(
+                      title: "problem e",
+                      imageAssetURL: "assets/scrumPhase/problem_e.png",
+                      action: () {},
                     ),
-                    new Column(
-                      children: <Widget>[
-                        new ScrumListBtn(
-                          title: "Problem E",
-                          action: () => {},
-                          imgUrl: "assets/scrumPhase/problem_e.png",
-                        ),
-                        new Text(
-                          "problem e",
-                          textAlign: TextAlign.center,
-                        )
-                      ],
-                    ),
-                    new Padding(
-                      padding: EdgeInsets.all(12),
-                    ),
-                    new Column(
-                      children: <Widget>[
-                        new ScrumListBtn(
-                          title: "Problem F",
-                          action: () => {},
-                          imgUrl: "assets/scrumPhase/problem_f.png",
-                        ),
-                        new Text(
-                          "problem f",
-                          textAlign: TextAlign.center,
-                        )
-                      ],
+                    new ScrumPhaseContentBtn(
+                      title: "problem f",
+                      imageAssetURL: "assets/scrumPhase/problem_f.png",
+                      action: () {},
                     ),
                   ],
                 ),
