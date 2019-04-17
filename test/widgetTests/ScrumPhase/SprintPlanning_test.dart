8548c4f894f50b7166ec5c136a06ed1d133d901d
Muhammad Rezki <iskarimrezki@gmail.com>
2019-04-17 19:24:47 +0700

    [GREEN] Fix SprintPlanning Test

--- test/widgetTests/ScrumPhase/SprintPlanning_test.dart
+++ test/widgetTests/ScrumPhase/SprintPlanning_test.dart
@@ -1,114 +1,165 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:http/http.dart';
+import 'package:http/testing.dart';
+import 'dart:convert';
+import 'package:image_test_utils/image_test_utils.dart';
 
+import 'package:ScrumBooster/ScrumPhase/SprintPlanning/ApiProvider.dart';
 import 'package:ScrumBooster/Utils/utils.dart';
-import 'package:ScrumBooster/ScrumPhase/SprintPlanning.dart';
+import 'package:ScrumBooster/ScrumPhase/SprintPlanning/SprintPlanning.dart';
 
 void main() {
-  final SprintPlanning sprintPlanning = SprintPlanning();
   final util = new Util();
+  final widgetTestingApiProvider = SprintPlanningApiProvider();
 
-  testWidgets('Find Header', (WidgetTester tester) async {
-    await tester.pumpWidget(util.makeTestableWidget(child: sprintPlanning));
-    String menu1 = "SPRINT PLANNING";
-    expect(find.text(menu1), findsOneWidget);
+  //Test Mocking Initialization
+  widgetTestingApiProvider.client = MockClient((request) async {
+    final mapJSON = util.getDummyJSONFile();
+    return Response(json.encode(mapJSON), 200);
   });
 
-  //Product Backlog Phase Widget Tests
+  final SprintPlanning sprintPlanning = SprintPlanning(
+    apiProvider: widgetTestingApiProvider,
+  );
+
+  //Sprint Planning Phase Widget Tests
+  testWidgets('Detect Loading Animation', (WidgetTester tester) async {
+    provideMockedNetworkImages(() async {
+      await tester.pumpWidget(
+          util.makeTestableWidget(child: sprintPlanning)
+      );
+      String key = "Loading Data";
+      expect(find.byKey(Key(key)), findsOneWidget);
+    });
+  });
+
+  testWidgets('Find Header', (WidgetTester tester) async {
+    provideMockedNetworkImages(() async {
+      await tester.pumpWidget(
+          util.makeTestableWidget(child: sprintPlanning)
+      );
+      await tester.pump();
+
+      String header = "SPRINT PLANNING";
+      expect(find.text(header), findsOneWidget);
+    });
+  });
+
   testWidgets('Test Find Product Header 1', (WidgetTester tester) async {
-    await tester.pumpWidget(util.makeTestableWidget(child: sprintPlanning));
-    String menu1 = "Things you should be doing:";
-    expect(find.text(menu1), findsOneWidget);
+    provideMockedNetworkImages(() async {
+      await tester.pumpWidget(
+          util.makeTestableWidget(child: sprintPlanning)
+      );
+      await tester.pump();
+      String menu1 = "Things you should be doing:";
+      expect(find.text(menu1), findsOneWidget);
+    });
   });
 
   testWidgets('Test Not Find Product Header 1', (WidgetTester tester) async {
-    await tester.pumpWidget(util.makeTestableWidget(child: sprintPlanning));
-    String menu1 = "Things you shouldn't be doing:";
-    expect(find.text(menu1), findsNothing);
+    provideMockedNetworkImages(() async {
+      await tester.pumpWidget(
+          util.makeTestableWidget(child: sprintPlanning)
+      );
+      await tester.pump();
+      String menu1 = "Things you shouldn't be doing:";
+      expect(find.text(menu1), findsNothing);
+    });
   });
 
   testWidgets('Test Find Product Header 2', (WidgetTester tester) async {
-    await tester.pumpWidget(util.makeTestableWidget(child: sprintPlanning));
-    String menu1 = "Problems you might have face:";
-    expect(find.text(menu1), findsOneWidget);
+    provideMockedNetworkImages(() async {
+      await tester.pumpWidget(
+          util.makeTestableWidget(child: sprintPlanning)
+      );
+      await tester.pump();
+      String menu1 = "Problems you might have face:";
+      expect(find.text(menu1), findsOneWidget);
+    });
   });
 
   testWidgets('Test Not Find Product Header 2', (WidgetTester tester) async {
-    await tester.pumpWidget(util.makeTestableWidget(child: sprintPlanning));
-    String menu1 = "Problems you might have not face:";
-    expect(find.text(menu1), findsNothing);
+    provideMockedNetworkImages(() async {
+      await tester.pumpWidget(
+          util.makeTestableWidget(child: sprintPlanning)
+      );
+      await tester.pump();
+      String menu1 = "Problems you might have not face:";
+      expect(find.text(menu1), findsNothing);
+    });
   });
 
-  testWidgets('Test Find Content Names', (WidgetTester tester) async {
-    //TODO: Ubah jadi nama judul content
-    List<String> contentNames = <String>[
-      "Backlog Grooming",
-      "content b",
-      "content c",
-      "content d",
-      "content e",
-      "content f",
-    ];
+  testWidgets('Test Find Ceremony Content Names', (WidgetTester tester) async {
+    provideMockedNetworkImages(() async {
+      List<String> contentNames = <String>[
+        "Backlog Grooming",
+      ];
 
-    await tester.pumpWidget(util.makeTestableWidget(child: sprintPlanning));
-    for (int i = 0; i < contentNames.length; i++) {
-      expect(find.byWidgetPredicate((widget) {
-        if (widget is Text) {
-          final Text textWidget = widget;
-          if (textWidget.data != null) {
-            return textWidget.data.contains(contentNames[i]);
+      await tester.pumpWidget(
+          util.makeTestableWidget(child: sprintPlanning)
+      );
+      await tester.pump();
+
+      for (int i = 0; i < contentNames.length; i++) {
+        expect(find.byWidgetPredicate((widget) {
+          if (widget is Text) {
+            final Text textWidget = widget;
+            if (textWidget.data != null) {
+              return textWidget.data.contains(contentNames[i]);
+            }
+            return textWidget.textSpan.toPlainText().contains(contentNames[i]);
           }
-          return textWidget.textSpan.toPlainText().contains(contentNames[i]);
-        }
-        return false;
-      }), findsOneWidget);
-    }
+          return false;
+        }), findsOneWidget);
+      }
+    });
   });
 
-  testWidgets('Test Find First 3 Problem Names', (WidgetTester tester) async {
-    //TODO: Ubah jadi nama judul problem
-    List<String> problemNames = <String>[
-      "Poor Project Communication",
-      "problem b",
-      "problem c",
-      "problem d",
-      "problem e",
-      "problem f",
-    ];
+  testWidgets('Test Find Problem Names', (WidgetTester tester) async {
+    provideMockedNetworkImages(() async {
+      List<String> problemNames = <String>[
+        "Project Estimates Are Unrealistic or Unknown",
+      ];
 
-    await tester.pumpWidget(util.makeTestableWidget(child: sprintPlanning));
-    for (int i = 0; i < 3; i++) {
-      expect(find.byWidgetPredicate((widget) {
-        if (widget is Text) {
-          final Text textWidget = widget;
-          if (textWidget.data != null) {
-            return textWidget.data.contains(problemNames[i]);
+      await tester.pumpWidget(
+        util.makeTestableWidget(child: sprintPlanning),
+      );
+      await tester.pump();
+
+      for (int i = 0; i < problemNames.length; i++) {
+        expect(find.byWidgetPredicate((widget) {
+          if (widget is Text) {
+            final Text textWidget = widget;
+            if (textWidget.data != null) {
+              return textWidget.data.contains(problemNames[i]);
+            }
+            return textWidget.textSpan.toPlainText().contains(problemNames[i]);
           }
-          return textWidget.textSpan.toPlainText().contains(problemNames[i]);
-        }
-        return false;
-      }), findsOneWidget);
-    }
+          return false;
+        }), findsOneWidget);
+      }
+    });
   });
 
-  // testWidgets('Home Screen Drawer Test', (WidgetTester tester) async {
-  //   GlobalKey<ScaffoldState> scaffoldKey = sprintPlanning.getScaffoldKey();
-  //   await tester.pumpWidget(util.makeTestableWidget(child: sprintPlanning));
-  //   Key menu1 = new Key("Home");
-  //   Key menu2 = new Key("Ceremonies");
-  //   Key menu3 = new Key("Problems");
-  //   Key menu4 = new Key("Glossary");
-  //   Key menu5 = new Key("Pop Quiz!");
-  //   Key menu6 = new Key("About");
-
-  //   scaffoldKey.currentState.openDrawer();
-  //   await tester.pump();
-
-  //   expect(find.byKey(menu1), findsOneWidget);
-  //   expect(find.byKey(menu2), findsOneWidget);
-  //   expect(find.byKey(menu3), findsOneWidget);
-  //   expect(find.byKey(menu4), findsOneWidget);
-  //   expect(find.byKey(menu5), findsOneWidget);
-  //   expect(find.byKey(menu6), findsOneWidget);
-  // });
+//  testWidgets('Home Screen Drawer Test', (WidgetTester tester) async {
+//    GlobalKey<ScaffoldState> scaffoldKey = sprintPlanning.getScaffoldKey();
+//    await tester.pumpWidget(util.makeTestableWidget(child: sprintPlanning));
+//    Key menu1 = new Key("Home");
+//    Key menu2 = new Key("Ceremonies");
+//    Key menu3 = new Key("Problems");
+//    Key menu4 = new Key("Glossary");
+//    Key menu5 = new Key("Pop Quiz!");
+//    Key menu6 = new Key("About");
+//
+//    scaffoldKey.currentState.openDrawer();
+//    await tester.pump();
+//
+//    expect(find.byKey(menu1), findsOneWidget);
+//    expect(find.byKey(menu2), findsOneWidget);
+//    expect(find.byKey(menu3), findsOneWidget);
+//    expect(find.byKey(menu4), findsOneWidget);
+//    expect(find.byKey(menu5), findsOneWidget);
+//    expect(find.byKey(menu6), findsOneWidget);
+//  });
 }
