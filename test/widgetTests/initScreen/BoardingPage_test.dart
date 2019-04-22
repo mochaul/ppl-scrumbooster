b37ddc0ccc6e435be1ae85c435773f64d2213575
Muhammad Rezki <iskarimrezki@gmail.com>
2019-04-22 17:44:04 +0700

    [GREEN] try to fix flutter test in boardingPgae_test

--- test/widgetTests/initScreen/BoardingPage_test.dart
+++ test/widgetTests/initScreen/BoardingPage_test.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:ScrumBooster/initScreen/BoardingPage.dart';
-import 'package:ScrumBooster/utils/utils.dart';
+import 'package:ScrumBooster/Utils/utils.dart';
 
 void main() {
   final TestWidgetsFlutterBinding binding =
