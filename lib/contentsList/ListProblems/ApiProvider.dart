bae624b49e624dcefe4da6d9ef5ddfea0c20ab4c
Izzan Fakhril Islam <izzan.fakhril@ui.ac.id>
2019-04-23 10:12:00 +0700

    Merge branch 'US-6-Sebagai_user_saya_ingin_mencari_dan_melihat_semua_konten_yang_ada' into 'Task-US-6-Integrasi_konten_list_semua_ceremonies'
    # Conflicts:
    #   lib/main.dart

---
+++ lib/contentsList/ListProblems/ApiProvider.dart
@@ -0,0 +1,31 @@
+import 'package:http/http.dart' show Client;
+import 'package:ScrumBooster/contentsList/ListProblems/Model.dart';
+import 'dart:convert';
+import 'package:ScrumBooster/Utils/utils.dart';
+
+class ListProblemsApiProvider {
+  Client client = Client();
+  var response;
+  var util = new Util();
+  ListProblemsModel model;
+  Map<String, List<ProblemItem>> dictProblemsAlphabetic;
+
+  fetchPosts() async {
+    response = await client.get(
+        util.getConfiguration()['base_url']+"problem/alphabetic/"
+    );
+    var jsonBody = json.decode(response.body);
+
+    model = ListProblemsModel.fromJson(jsonBody);
+    dictProblemsAlphabetic = model.dictProblemsAlphabetic;
+
+  }
+
+  ListProblemsModel getModel() {
+    return model;
+  }
+
+  Map<String, List<ProblemItem>> getDictProblemsAlphabetic() {
+    return dictProblemsAlphabetic;
+  }
+}
