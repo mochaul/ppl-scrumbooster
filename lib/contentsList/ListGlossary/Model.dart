c74b6f655d04060cb017b9d8bd423e33c93c3727
Muhammad Rezki <iskarimrezki@gmail.com>
2019-04-29 16:54:35 +0700

    [GREEN] remove phase in glossary model

--- lib/contentsList/ListGlossary/Model.dart
+++ lib/contentsList/ListGlossary/Model.dart
@@ -22,19 +22,16 @@ class GlossaryItem {
   String _title;
   String _detail;
   String _image;
-  int _phase;
 
   GlossaryItem.fromJson(Map<String, dynamic> parsedJson) {
     _id = parsedJson['id'];
     _title = parsedJson['title'];
     _detail = parsedJson['detail'];
     _image = parsedJson['image'];
-    _phase = parsedJson['phase'];
   }
 
   int get id => _id;
   String get title => _title;
   String get detail => _detail;
   String get image => _image;
-  int get phase => _phase;
 }
